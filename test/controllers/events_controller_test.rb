30305b53fe905e123086a8962df73bfe2cdde6c2
Anastasia <anastasiamaschenko103@gmail.com>
2019-04-05 00:29:10 +0300

    show one event

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,6 +1,11 @@
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  test 'show should return success' do
+  test 'index should return success' do
     get events_path
+    assert_response :success
+  end
+
+  test 'show should return success' do
+    get event_path(events(:one).id)
     assert_response :success
   end
 end
